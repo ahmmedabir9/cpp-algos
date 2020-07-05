f4981bd59edcbde51af6760e0c1f22213815f0d3
Ahmmed Abir <ahmmedabir9@gmail.com>
2020-07-05 11:50:54 +0600

    Sorting Added

---
+++ Sorting/Insertion-Sort.cpp
@@ -0,0 +1,68 @@
+// Abu Musa Ahmed ABIR!!
+
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define fo(i,n) for(i=0;i<n;i++)
+#define mkp make_pair
+#define maxx 1000000005
+#define MOD 1000003
+#define mem(a) memset(a, 0, sizeof(a))
+#define pi 3.14159265358979323846
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pii(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define out(x) cout << x << endl
+#define all(x) x.begin(), x.end()
+#define empty -1
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pr;
+typedef vector<ll> vll;
+
+
+void insertionSort(int arr[], int n)
+{
+	int i, key, j;
+	for (i = 1; i < n; i++)
+	{
+		key = arr[i];
+		j = i - 1;
+		//for increasing '>'
+		//for decreasing '<'
+		while (j >= 0 && arr[j] > key)
+		{
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
+}
+
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+
+	int n, i;
+	cin>>n;
+	int arr[n];
+	fo(i, n)
+	{
+		cin>>arr[i];
+	}
+	insertionSort(arr, n);
+
+	fo(i, n)
+	{
+		cout<<arr[i]<<" ";
+	}
+
+	return 0;
+}
---
+++ Sorting/Merge-Sort.cpp
@@ -0,0 +1,93 @@
+// Abu Musa Ahmed ABIR!!
+
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define fo(i,n) for(i=0;i<n;i++)
+#define mkp make_pair
+#define maxx 1000000005
+#define MOD 1000003
+#define mem(a) memset(a, 0, sizeof(a))
+#define pi 3.14159265358979323846
+#define si(x)   scanf("%d",&x)
+#define sl(x)   scanf("%lld",&x)
+#define ss(s)   scanf("%s",s)
+#define pii(x)  printf("%d\n",x)
+#define pl(x)   printf("%lld\n",x)
+#define ps(s)   printf("%s\n",s)
+#define out(x) cout << x << endl
+#define all(x) x.begin(), x.end()
+#define empty -1
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pr;
+typedef vector<ll> vll;
+
+
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)    L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)    R[j] = arr[m + 1 + j];
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+int main()
+{
+    int n, i;
+    cin >> n;
+    int arr[n];
+    fo(i, n)
+    {
+        cin >> arr[i];
+    }
+    mergeSort(arr, 0, n);
+
+    fo(i, n)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
